45a902169c25049b40587f3ccb7919992603f25e
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-05-07 22:31:34 -0600

    Worked on basic structure, working on dependencies

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "rootroot",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+
+});
+
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,17 @@
+const connection = require("./config/connection");
+
+// Dependencies
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
